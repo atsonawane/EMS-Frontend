9ff1e99b9fb4b90fb5ccaf0383ad2ea054f3991b
Avinash <avinashsonawane55@gmail.com>
2024-01-12 03:56:28 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { ListEmployeeComponent } from './components/ListEmployeeComponent'
+
+function App() {
+
+  return (
+    <>
+          <ListEmployeeComponent></ListEmployeeComponent>
+    </>
+  )
+}
+
+export default App
---
+++ src/services/EmployeeService.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const REST_API_BASE_URL = 'http://localhost:8080/employee';
+
+export const listEmployees = () => axios.get(REST_API_BASE_URL);
+
+
+
+
+ 
